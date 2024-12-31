ca0a777e505ef0a33ee65d6b96561293e622339d
Reza <mail@local>
2024-12-31 15:50:35 +0330

    Edit backup script

--- bck.sh
+++ bck.sh
@@ -13,12 +13,10 @@ fi
 
 # Database credentials and details
 DB_HOST="db" #name of the mysql container
-DB_USER="$MYSQL_USER"
-DB_PASSWORD="$MYSQL_PASSWORD"
-DB_NAME="wordpress"
-NETWORK="internal-network" #name of the network where mysql container is running. You can check the list of the docker neworks using doocker network ls
-
-# Docker image version of MySQL
+DB_USER="wpuser" #name of the database user
+DB_PASSWORD="5sad4f54sdaf65sdf465sdaf6fsdeeee" #DB_USER password
+DB_NAME="wordpress" #database name
+NETWORK="internal-network" # name of the network where mysql container is running
 MYSQL_IMAGE="mysql:8.0"
 
 # Backup filename
